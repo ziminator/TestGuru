649e32bee2742eb7dfbde4b0e02fdc78786993a7
ziminator <aleksander.zimin@gmail.com>
2019-04-30 07:25:19 +0300

    TestGuru

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,6 @@
+class QuestionsController < ApplicationController
+
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get 'main/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
+
+  resources :tests do
+    resources :questions, shallow: true
+  end
 end
