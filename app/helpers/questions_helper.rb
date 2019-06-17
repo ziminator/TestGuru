3820f97f7011e85a33c6aab4e902ccc006df8649
ziminator <aleksander.zimin@gmail.com>
2019-06-17 07:47:46 +0300

    TestGuru_8

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,6 @@
 module QuestionsHelper
+
+  def question_header
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  #get 'questions/new'
-  get 'main/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
+  #get 'questions/new'
+  get '/index', to: 'main#index'
+  get '/new', to: 'questions#new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :tests do
     resources :questions, shallow: true
