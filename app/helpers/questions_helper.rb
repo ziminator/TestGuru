c91b665841f2ea6a63f5e9b61409f471ca902540
ziminator <aleksander.zimin@gmail.com>
2019-06-21 07:46:33 +0300

    Fix Lesson-8 after codereview

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -8,12 +8,4 @@ module QuestionsHelper
     end
   end
 
-  def current_year
-    Time.current.year
-  end
-
-  def github_url(author, repo)
-    link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank'
-  end
-
 end
