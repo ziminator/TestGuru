9f51f3579e37b80bb78e9bb1bd53835a095cbca4
ziminator <aleksander.zimin@gmail.com>
2019-06-25 00:25:15 +0300

    Start Lesson-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   root 'tests#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :tests do
-    resources :questions, shallow: true, except: :index
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
 end
