4b5ed41dc2003398ff0605b36ea901e72d91c18a
Ziminator <aleksander.zimin@gmail.com>
2019-06-26 02:44:00 -0700

    Leddon-9 finished

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,6 +5,9 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on :create
+  before_validation :before_validation_set_next_question, on :update
+
+  PASS_VALUE = 85
 
   def completed?
     current_question.nil?
@@ -19,25 +22,34 @@ class TestPassage < ApplicationRecord
     save!
   end
 
+  def success?
+    success_value >= PASS_VALUE
+  end
+
+  def success_value
+    correct_questions.to_f / test.questions.count.to_f * 100
+  end
+
+  def question_num
+    test.questions.order(id: :asc).find_index(current_question) + 1
+  end
+
   private
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
   end
 
-  def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
+  def before_validation_set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+  end
 
-    (correct_answers.count == correct_answers.where(id: answer_ids).count) &&
-    correct_answers_count == answer_ids.count
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.correct
-  end
-
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
 end
