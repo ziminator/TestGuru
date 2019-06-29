e009fd96a7195ccb2f00c45f5097f6c112bbb34f
ziminator <aleksander.zimin@gmail.com>
2019-06-29 08:24:43 +0300

    Lesson-11 first part

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,6 +2,7 @@ class TestsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :find_test, only: %i[show edit update destroy start]
+  before_action :set_user, only: :start
 
  def index
     @tests = Test.all
