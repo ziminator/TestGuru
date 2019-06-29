f786eadf5487dd1c6cb4e038a1892eaa635e82f1
ziminator <aleksander.zimin@gmail.com>
2019-06-29 09:04:20 +0300

    Lesson-11 second part

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to cookies[:path] || root_path
     else
-      flash.now[:alert] = 'Are you a Guru? Verify your Email and Password please.'
+      flash.now[:alert] = 'Verify your Email and Password please.'
       render :new
     end
   end
