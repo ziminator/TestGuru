13c0cfd315705ac5e89835c2dee02afd13a1dba7
Ziminator <aleksander.zimin@gmail.com>
2019-07-18 09:04:45 -0700

    Lesson-17 fix js

--- app/assets/javascripts/utilities/password_matching.js
+++ app/assets/javascripts/utilities/password_matching.js
@@ -1,10 +1,10 @@
 document.addEventListener('turbolinks:load', function() {
-    var password = document.getElementById('user_password');
-    var password_confirmation = document.getElementById('user_password_confirmation');
+    var password = document.getElementById('user_password')
+    var password_confirmation = document.getElementById('user_password_confirmation')
 
     if (password && password_confirmation) {
-        password.addEventListener('input', checkPassword);
-        password_confirmation.addEventListener('input', checkPasswordConfirmation);
+        password.addEventListener('input', checkPassword)
+        password_confirmation.addEventListener('input', checkPasswordConfirmation)
     }
 })
 
@@ -13,29 +13,29 @@ function checkPassword() {
 }
 
 function checkPasswordConfirmation() {
-    var password = document.getElementById('user_password');
+    var password = document.getElementById('user_password')
 
     if (password.value) {
-        (this.value == password.value) ? iconSuccess() : iconDanger();
+        (this.value == password.value) ? iconSuccess() : iconDanger()
     }
 
     if (!this.value) { iconHide() }
 }
 
 function iconSuccess() {
-    document.querySelector('.octicon-key').classList.remove('hide');
-    document.querySelector('.octicon-key').classList.remove('text-danger');
-    document.querySelector('.octicon-key').classList.add('text-success');
+    document.querySelector('.octicon-key').classList.remove('hide')
+    document.querySelector('.octicon-key').classList.remove('text-danger')
+    document.querySelector('.octicon-key').classList.add('text-success')
 }
 
 function iconDanger() {
-    document.querySelector('.octicon-key').classList.remove('hide');
-    document.querySelector('.octicon-key').classList.remove('text-success');
-    document.querySelector('.octicon-key').classList.add('text-danger');
+    document.querySelector('.octicon-key').classList.remove('hide')
+    document.querySelector('.octicon-key').classList.remove('text-success')
+    document.querySelector('.octicon-key').classList.add('text-danger')
 }
 
 function iconHide() {
-    document.querySelector('.octicon-key').classList.remove('text-success');
-    document.querySelector('.octicon-key').classList.remove('text-danger');
-    document.querySelector('.octicon-key').classList.add('hide');
+    document.querySelector('.octicon-key').classList.remove('text-success')
+    document.querySelector('.octicon-key').classList.remove('text-danger')
+    document.querySelector('.octicon-key').classList.add('hide')
 }
