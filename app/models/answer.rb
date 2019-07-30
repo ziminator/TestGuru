e560811c14d768a3f643179b93b3a52cbbec137a
Ziminator <aleksander.zimin@gmail.com>
2019-07-30 08:13:33 -0700

    Lesson-17 fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,7 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :answer, presence: true
-  validate :max_answer_count, on: :create
+  #validate :max_answer_count, on: :create
 
   scope :correct, -> { where(correct: true) }
 
